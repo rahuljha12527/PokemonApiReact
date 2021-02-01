1239ffa78bc0d5d019b3b6942e27a1409af788d7
rahuljha661 <rahuljha12527@gmail.com>
2021-02-01 17:28:00 +0530

    renderPokemonName

---
+++ src/PokemonList.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+
+export default function PokemonList({pokemon}) {
+   
+    return (
+        <div>
+            {pokemon.map(p=>(   
+                <div key={p}>{p}</div>
+            ))}
+        </div>
+    )
+}
